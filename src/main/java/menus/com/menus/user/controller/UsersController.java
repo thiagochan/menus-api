1d8d885359f2770262dfd052c5f56957d5b8e461
thiagochan <thcooficial@gmail.com>
2025-03-22 16:41:11 -0300

    fix: user responses

--- src/main/java/menus/com/menus/user/controller/UsersController.java
+++ src/main/java/menus/com/menus/user/controller/UsersController.java
@@ -7,6 +7,7 @@ import menus.com.menus.user.domain.dtos.UserLoginForm;
 import menus.com.menus.user.domain.entities.Users;
 import menus.com.menus.user.service.UsersMapper;
 import menus.com.menus.user.service.UsersService;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -19,24 +20,29 @@ public class UsersController {
     private final UsersMapper usersMapper;
 
     @PostMapping
-    public ResponseEntity<Void> create(@RequestBody UserCreateForm form) {
+    public ResponseEntity<String> create(@RequestBody UserCreateForm form) {
+        Users hasUser = usersService.getUserByEmail(form.getEmail());
+        if (hasUser != null) {
+            return ResponseEntity.status(HttpStatus.CONFLICT).body("E-mail já existente");
+        }
+
         String hash = usersService.passwordHash(form.getPassword());
         Users userToSave = usersMapper.convert(form, hash);
         usersService.save(userToSave);
-        return ResponseEntity.ok().build();
+        return new ResponseEntity<>("Usuário criado com sucesso", HttpStatus.CREATED);
     }
 
     @GetMapping("/login")
-    public ResponseEntity<Void> login(@RequestBody UserLoginForm form) {
+    public ResponseEntity<String> login(@RequestBody UserLoginForm form) {
         Users userToLogin = usersService.getUserByEmail(form.getEmail());
 
         if (userToLogin == null) {
-            return ResponseEntity.notFound().build();
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Usuário não encontrado");
         }
 
         String formHash = usersService.passwordHash(form.getPassword());
         if (!formHash.equals(userToLogin.getPassword())) {
-            return ResponseEntity.badRequest().build();
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Senha inválida");
         }
 
         return ResponseEntity.ok().build();
